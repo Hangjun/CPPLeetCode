594b3355a56e3f140cafee6bb87e67e6ac3dbf81
Hangjun <ajxuhangjun2007@gmail.com>
2015-10-23 16:59:49 -0700

    Create 005_Longest_Palindromic_Substring.cpp

---
+++ 005_Longest_Palindromic_Substring.cpp
@@ -0,0 +1,36 @@
+Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+
+Example
+
+Given the string = "abcdzdcab", return "cdzdc".
+Challenge
+
+O(n2) time is acceptable. Can you do it in O(n) time.
+
+/* Dynamic Programming Solution: O(n^2) Time, O(n^2) Space: Time Limit Exceeded by LeetCode OJ */
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int len = s.size();
+        if (len <= 1)  return s;
+        // dp[i][j]: whether s[i,...,j] is palindrome
+        vector<vector<bool>> dp(len, vector<bool>(len, false));
+        int maxLen = 1;
+        int start = 0;
+        for (int i = len-1; i >= 0; i--) {
+            for (int j = i; j < len; j++) {
+                if ((j-i<=2 || dp[i+1][j-1] == true) && s[i] == s[j]) {
+                    dp[i][j] = true;
+                    if (j-i+1 > maxLen) {
+                        maxLen = j-i+1;
+                        start = i;
+                    }
+                }
+            }
+        }
+        // now the maxLen palindrome substring starts at i
+        return s.substr(start, maxLen);
+    }
+};
+
+
