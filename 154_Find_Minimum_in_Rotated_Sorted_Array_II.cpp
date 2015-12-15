ff821d99aa1a924ceab2df737d03770730b58362
Hangjun <ajxuhangjun2007@gmail.com>
2015-12-14 20:40:44 -0800

    Update 154_Find_Minimum_in_Rotated_Sorted_Array_II.cpp

--- 154_Find_Minimum_in_Rotated_Sorted_Array_II.cpp
+++ 154_Find_Minimum_in_Rotated_Sorted_Array_II.cpp
@@ -10,6 +10,14 @@ Find the minimum element.
 The array may contain duplicates.
 */
 
+/*
+This problem can be solved in the same way as its predecessor Find Minimum in Rotated Sorted Array, except that now it is possible that 
+\verb+nums[mid] == nums[end]+, in which case we do not know if the minimum is before \verb+mid+ or after \verb+mid+. How do we proceed? 
+One simple idea is to decrement \verb+end+ by $1$ and ignore the last element. Even if the last element is indeed minimal, since we still 
+hold \verb+mid+, the minimal is not lost. 
+Thus we add a new test condition for this case:
+*/
+
 class Solution {
 public:
     int findMin(vector<int>& nums) {
