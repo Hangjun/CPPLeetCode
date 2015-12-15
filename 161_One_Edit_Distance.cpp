a4b216e1690b367db07df8724637cb986bd70758
Hangjun <ajxuhangjun2007@gmail.com>
2015-12-14 20:18:36 -0800

    Update 161_One_Edit_Distance.cpp

--- 161_One_Edit_Distance.cpp
+++ 161_One_Edit_Distance.cpp
@@ -5,21 +5,26 @@ Given two strings S and T, determine if they are both one edit distance apart.
 class Solution {
 public:
     bool isOneEditDistance(string s, string t) {
-        if (s.size() > t.size())
-            swap(s, t);
+        // make the first string the one with smaller size
+        if (s.size() > t.size()) {
+            return isOneEditDistance(t, s);
+        }
+        // check if the size differ by more than one
         if (t.size() - s.size() > 1) return false;
-        bool have = false;
+        bool haveDiffered = false;
         for (int i = 0, j = 0; i < s.size(); i++, j++) {
             if (s[i] != t[j]) {
-                if (have) {
-                    return false;
-                }
-                have = true;
+                if (haveDiffered) return false;
+                haveDiffered = true;
+                // if s.size() < t.size() (as opposed to s.size() == t.size(),
+                // we need to make i stay by decrementing it by one
                 if (s.size() < t.size()) {
                     i--;
                 }
             }
         }
-        return have || (s.size() < t.size());
+        // caution: at this point if s.size() == t.size() && !haveDiffered, then 
+        // s and t are identical: need to report false;
+        return haveDiffered || s.size() < t.size();
     }
 };
