e56da0e0596748c1af2e2469b6c49a643ec9d3ee
Hangjun <ajxuhangjun2007@gmail.com>
2016-01-11 22:17:08 -0800

    Create 326_Power_of_Three.cpp

---
+++ 326_Power_of_Three.cpp
@@ -0,0 +1,42 @@
+/*
+Given an integer, write a function to determine if it is a power of three.
+
+Follow up:
+Could you do it without using any loop / recursion? 
+*/
+
+// Submission #1: While-loop solution. Accepted
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        // corner case
+        if (n <= 0) return false;
+        if (n == 1) return true;
+        
+        // repeatedly divide n by 3
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+};
+
+// Submission #2: O(1)
+// Idea: Compute the largest integer that is a power of 3. Then n is a power of 3 iff n divides it. 
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        // corner case
+        if (n <= 0) return false;
+        if (n == 1) return true;
+        
+        // compute the largest integer that is a power of 3
+        int maxPow3 = log10(INT_MAX) / log10(3);
+        int maxPow3Val = pow(3, maxPow3);  
+
+        // n is a power of 3 iff n divides maxPow3Val
+        return maxPow3Val % n == 0;
+    }
+};
