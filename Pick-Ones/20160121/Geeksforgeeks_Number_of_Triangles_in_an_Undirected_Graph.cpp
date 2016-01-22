63cff9e84eaf2e9b3abc5e2c53bc71b6a31664a9
Hangjun <ajxuhangjun2007@gmail.com>
2016-01-22 12:06:23 -0800

    Update Geeksforgeeks_Number_of_Triangles_in_an_Undirected_Graph.cpp

--- Pick-Ones/20160121/Geeksforgeeks_Number_of_Triangles_in_an_Undirected_Graph.cpp
+++ Pick-Ones/20160121/Geeksforgeeks_Number_of_Triangles_in_an_Undirected_Graph.cpp
@@ -3,11 +3,17 @@ Given an Undirected simple graph, We need to find how many triangles it can have
 */
 
 /* Analysis:
+The bruteforce solution is to check all triples (i,j,k) of the graph nodes, and if there is an between from i to j, and j to k and k to i, 
+we increment a counter. We need to divied the result by 6 since the we are triple counted the nodes in a triangle, and since the graph is 
+undirected so (i,j,k) and (k,j,i) are the same. This algorithm runs in $O(|V|^3)$ time, where $V$ is the vertex set. Can we do better than 
+cubic running time?
+
 Let adj be the adjancency matrix of an undirected graph. adj[i][j] = k if there are k edges between i and j. Another way to think about 
 this is that adj[i][j] counts the number of length 1 paths between i and j. It turns out that adj^n[i][j] counts the number of length n 
 paths between i and j. Therefore for this problem, we are simply interested in adj^3[i][i]: number of length 3 paths between any node i and 
 itself, i.e. triangles. The sum \sum_{i}adj^3[i][i] = trace(adj^3) has triple counts the nodes in a triagle. Modulo the undirectedness the 
-result is trace(adj^3)/6.
+result is trace(adj^3)/6. Strassen’s matrix multiplication algorithm runs in O(|V|^2.8074) time, thus this approach can beat the cubic running 
+time in the brute-force algorithm. We will not implement Strassen's algorithm for this problem.
 */
 
 #include <iostream>
