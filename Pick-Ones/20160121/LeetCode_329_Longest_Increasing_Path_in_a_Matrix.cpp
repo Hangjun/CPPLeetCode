477486e17c4d2c50803a7a9c818e19bd6ddf9332
Hangjun <ajxuhangjun2007@gmail.com>
2016-01-22 21:06:59 -0800

    Update LeetCode_329_Longest_Increasing_Path_in_a_Matrix.cpp

--- Pick-Ones/20160121/LeetCode_329_Longest_Increasing_Path_in_a_Matrix.cpp
+++ Pick-Ones/20160121/LeetCode_329_Longest_Increasing_Path_in_a_Matrix.cpp
@@ -250,6 +250,49 @@ private:
 };
 
 
-
+// In fact, the following also works:
+// Submission #5: Accepted:
+class Solution {
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) return 0;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        // dp[i][j] = length of longest increasing path that STARTS from (i,j)
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        int maxLen = INT_MIN;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // dfs returns maximum length of the increasing path that reaches (i, j)
+                int curLen = dfs(matrix, i, j, dp);
+                maxLen = max(maxLen, curLen);
+            }
+        }
+        return maxLen;
+    }
+    
+private:
+    int dx[4] = {0, 1, 0, -1};
+    int dy[4] = {-1, 0, 1, 0};
+    
+    int dfs(vector<vector<int>> &matrix, int x, int y, vector<vector<int>> &dp) {
+        //if (dp[x][y] != 0) return dp[x][y];
+        if (dp[x][y] != -1) return dp[x][y];
+        
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (inbound(matrix, nx, ny) && matrix[nx][ny] > matrix[x][y]) {
+                // route (x, y) to go through (nx, ny) to get a longer path
+                dp[x][y] = max(dp[x][y], 1 + dfs(matrix, nx, ny, dp));
+            }
+        }
+        return dp[x][y];
+    }
+    
+    bool inbound(vector<vector<int>> &matrix, int x, int y) {
+        return (x >= 0 && x < matrix.size() && y >= 0 && y < matrix[0].size()); 
+    }
+};
 
 
