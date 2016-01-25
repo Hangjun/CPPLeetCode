78776b8e23ecc6d28fe1be3299682f0a0059b541
Hangjun <ajxuhangjun2007@gmail.com>
2016-01-24 22:09:53 -0800

    Create LeetCode_56_Merge_Intervals.cpp

---
+++ Pick-Ones/20160124/LeetCode_56_Merge_Intervals.cpp
@@ -0,0 +1,38 @@
+/*
+Given a collection of intervals, merge all overlapping intervals.
+
+For example,
+Given [1,3],[2,6],[8,10],[15,18],
+return [1,6],[8,10],[15,18]. 
+*/
+
+// Submission #1: Accepted.
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        vector<Interval> res;
+        if (intervals.empty()) return res;
+        sort(intervals.begin(), intervals.end(), [](Interval i1, Interval i2){return i1.start < i2.start || i1.start == i2.start && i1.end < i2.end;});
+        Interval lastInterval = intervals[0];
+        for (int i = 1; i < intervals.size(); i++) {
+            if (lastInterval.end >= intervals[i].start && lastInterval.start <= intervals[i].end) {
+                lastInterval.start = min(lastInterval.start, intervals[i].start);
+                lastInterval.end = max(lastInterval.end, intervals[i].end);
+            } else {
+                res.push_back(lastInterval);
+                lastInterval = intervals[i];
+            }
+        }
+        res.push_back(lastInterval);
+        return res;
+    }
+};
