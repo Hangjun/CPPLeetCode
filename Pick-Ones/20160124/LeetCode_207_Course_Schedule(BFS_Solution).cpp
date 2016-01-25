57e1b8f9eeb5009df38a9530969cacd1be313de3
Hangjun <ajxuhangjun2007@gmail.com>
2016-01-24 20:51:47 -0800

    Update LeetCode_207_Course_Schedule(BFS_Solution).cpp

--- Pick-Ones/20160124/LeetCode_207_Course_Schedule(BFS_Solution).cpp
+++ Pick-Ones/20160124/LeetCode_207_Course_Schedule(BFS_Solution).cpp
@@ -52,7 +52,7 @@ public:
             }
         }
         if (prereqCourses.empty()) return false;
-        while (totalDegree != 0 && !prereqCourses.empty()) {
+        while (!prereqCourses.empty()) {
             int curCourse = prereqCourses.front();
             prereqCourses.pop();
             for (int i = 0; i < prerequisites.size(); i++) {
