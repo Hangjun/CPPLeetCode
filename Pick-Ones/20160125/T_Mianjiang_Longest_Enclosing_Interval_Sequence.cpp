0c515d3119f357b7c30e251433eec5e536ee92f1
Hangjun <ajxuhangjun2007@gmail.com>
2016-01-25 17:10:00 -0800

    Create T_Mianjiang_Longest_Enclosing_Interval_Sequence.cpp

---
+++ Pick-Ones/20160125/T_Mianjiang_Longest_Enclosing_Interval_Sequence.cpp
@@ -0,0 +1,6 @@
+/*
+Given a list of intervals, return the longest enclosing interval sequence in which the ith interval encloses the (i+1)th interval.
+
+Example: Given [[-100, 100], [-5, 10], [1, 5], [-5, 10]], return [[-100, 100], [-5, 10], [1, 5]], length = 3.
+*/
+
