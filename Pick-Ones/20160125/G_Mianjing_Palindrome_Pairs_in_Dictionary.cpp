8b586ec75a5ce61492ad07af961429864493001b
Hangjun <ajxuhangjun2007@gmail.com>
2016-01-25 17:07:33 -0800

    Create G_Mianjing_Palindrome_Pairs_in_Dictionary.cpp

---
+++ Pick-Ones/20160125/G_Mianjing_Palindrome_Pairs_in_Dictionary.cpp
@@ -0,0 +1,10 @@
+/*
+Given a huge dictionary with distinct strings, find all pairs that could make a palindrome string when combined.
+
+Example: Given [abcd, dcba, lls, s, sssll], return:
+[abcd, dcba]
+[lls, s]
+[lls, sssll]
+*/
+
+
