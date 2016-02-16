3255bda3c63f971859273ecbb3b72a5c5f785669
Hangjun <ajxuhangjun2007@gmail.com>
2016-02-16 14:09:32 -0800

    Update 257_Binary_Tree_Paths.cpp

--- 257_Binary_Tree_Paths.cpp
+++ 257_Binary_Tree_Paths.cpp
@@ -22,6 +22,92 @@ Several key points:
 3. When we return from a leaf node, we need delete the leaf node from our local path and back track to its parent.
 */
 
+
+// DFS version #1: push after recurse, clean up before returning.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> res;
+        if (!root) return res;
+        vector<TreeNode *> path;
+        binaryTreePathsDFS(root, path, res);
+        return res;
+    }
+    
+    void binaryTreePathsDFS(TreeNode *curNode, vector<TreeNode *> &path, vector<string> &res) {
+        path.push_back(curNode);
+        // terminating condition
+        if (curNode->left == NULL && curNode->right == NULL) {
+            // convert path into string
+            string s = "";
+            int i;
+            for (i = 0; i < path.size()-1; i++) {
+                s += to_string(path[i]->val) + "->";
+            }
+            s += to_string(path[i]->val);
+            res.push_back(s);
+            path.pop_back();
+            return;
+        }
+        
+        if (curNode->left) binaryTreePathsDFS(curNode->left, path, res);
+        if (curNode->right) binaryTreePathsDFS(curNode->right, path, res);
+
+        path.pop_back();
+    }
+};
+
+// DFS version 1': Local path pass by copy trick used.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> res;
+        if (!root) return res;
+        vector<TreeNode *> path;
+        binaryTreePathsDFS(root, path, res);
+        return res;
+    }
+    
+    void binaryTreePathsDFS(TreeNode *curNode, vector<TreeNode *> path, vector<string> &res) {
+        path.push_back(curNode);
+        // terminating condition
+        if (curNode->left == NULL && curNode->right == NULL) {
+            // convert path into string
+            string s = "";
+            int i;
+            for (i = 0; i < path.size()-1; i++) {
+                s += to_string(path[i]->val) + "->";
+            }
+            s += to_string(path[i]->val);
+            res.push_back(s);
+            return;
+        }
+        
+        if (curNode->left) binaryTreePathsDFS(curNode->left, path, res);
+        if (curNode->right) binaryTreePathsDFS(curNode->right, path, res);
+    }
+};
+
+
+// DFS version #2: push before recurse, clean up after returning.
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
