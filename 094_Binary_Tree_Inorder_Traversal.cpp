3f4c3596b1a5d06212af0cf718645e7fd414b35b
Hangjun <ajxuhangjun2007@gmail.com>
2016-03-20 17:27:26 -0700

    Update 094_Binary_Tree_Inorder_Traversal.cpp

--- 094_Binary_Tree_Inorder_Traversal.cpp
+++ 094_Binary_Tree_Inorder_Traversal.cpp
@@ -40,7 +40,39 @@ public:
                 curNode = tmpNode->right;
             }
         }
-        
+        return res;
+    }
+};
+
+/* Morris traversal solution: O(n) time, O(1) space */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        // left-mid-right
+        vector<int> res;
+        if (!root) return res;
+        stack<TreeNode *> st;
+        TreeNode *curNode = root;
+        while (curNode || !st.empty()) {
+            if (curNode) {
+                st.push(curNode);
+                curNode = curNode->left;
+            } else { // curNode == NULL, backtrack
+                TreeNode *tmpNode = st.top();
+                st.pop();
+                res.push_back(tmpNode->val);
+                curNode = tmpNode->right;
+            }
+        }
         return res;
     }
 };
