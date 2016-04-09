b110746100e41177fbb45c015bc2219d992bb3a3
Hangjun <ajxuhangjun2007@gmail.com>
2016-04-08 21:47:35 -0700

    Update 008_String_to_Integer_(atoi).cpp

--- 008_String_to_Integer_(atoi).cpp
+++ 008_String_to_Integer_(atoi).cpp
@@ -7,6 +7,17 @@ Notes: It is intended for this problem to be specified vaguely (ie, no given inp
 
 Update (2015-02-10):
 The signature of the C++ function had been updated. If you still see your function signature accepts a const char * argument, please click the reload button to reset your code definition.
+
+Requirements for atoi:
+
+The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+
+The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+
+If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+
+If no valid conversion could be performed, a zero value is returned. If the correct value is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
+
 */
 
 class Solution {
