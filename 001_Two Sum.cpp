7d188df671bd6f3ea21adf949e5b905cd898bc50
Hangjun <ajxuhangjun2007@gmail.com>
2016-04-10 23:17:24 -0700

    Create 001_Two Sum.cpp

---
+++ 001_Two Sum.cpp
@@ -0,0 +1,39 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+UPDATE (2016/2/13):
+The return format had been changed to zero-based indices. Please read the above updated description carefully. 
+*/
+
+/* Hash table solution:
+Time: O(n), one pass
+Space: O(n)
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        unordered_map<int, int> hmap;
+        for (int i = 0; i < nums.size(); i++){
+            if (hmap.find(target - nums[i]) == hmap.end()) {
+                hmap[nums[i]] = i;
+            } else {
+                int index1 = hmap[target - nums[i]] < i ? hmap[target - nums[i]] : i;
+                int index2 = hmap[target - nums[i]] > i ? hmap[target - nums[i]] : i;
+                result.push_back(index1);
+                result.push_back(index2);
+                return result;
+            }
+        }
+    }
+};
