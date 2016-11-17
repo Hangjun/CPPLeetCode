44acc8aa0cc3eb854172eec5718bd6c95dd3dde6
Hangjun <ajxuhangjun2007@gmail.com>
2016-11-16 23:26:31 -0800

    Update 398_Random_Pick_Index.cpp

--- 398_Random_Pick_Index.cpp
+++ 398_Random_Pick_Index.cpp
@@ -26,7 +26,10 @@ public:
         int res = -1;
         if (nums.empty()) return res;
         int count = 0;
-        for (int i = 0; i < nums.size(); i++) {
+        int i = 0;
+        while (nums[i] != target) i++;
+        res = i;
+        for (; i < nums.size(); i++) {
             if (nums[i] != target) continue;
             ++count;
             if (rand() % count == 0) res = i;
@@ -36,12 +39,6 @@ public:
 private:
     vector<int> nums;
 };
-
-/**
- * Your Solution object will be instantiated and called as such:
- * Solution obj = new Solution(nums);
- * int param_1 = obj.pick(target);
- */
 
 /**
  * Your Solution object will be instantiated and called as such:
