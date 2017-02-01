b1af6303247c073480d8db6950b0b9575e131b77
Hangjun <ajxuhangjun2007@gmail.com>
2017-01-31 23:59:16 -0800

    Update 205_Isomorphic_Strings.cpp

--- 205_Isomorphic_Strings.cpp
+++ 205_Isomorphic_Strings.cpp
@@ -61,3 +61,21 @@ public:
         return true;
     }+
 };
+
+// Solution #3: Without using hash table.
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.size() != t.size()) return false;
+        vector<int> map_s(128, 0);
+        vector<int> map_t(128, 0);
+        int len = s.size();
+        for (int i = 0; i < len; i++) {
+            if (map_s[s[i]] != map_t[t[i]]) return false;
+            map_s[s[i]] = i+1;
+            map_t[t[i]] = i+1;
+        }
+        
+        return true;
+    }
+};
