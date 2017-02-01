26c5ab481f5445685f085f7ba14d6a6b5473b3b5
Hangjun <ajxuhangjun2007@gmail.com>
2017-01-31 23:10:49 -0800

    Create 243_Shortest_Word_Distance.cpp

---
+++ 243_Shortest_Word_Distance.cpp
@@ -0,0 +1,35 @@
+/*
+Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+For example,
+Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+Given word1 = “coding”, word2 = “practice”, return 3.
+Given word1 = "makes", word2 = "coding", return 1.
+
+Note:
+You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
+*/
+
+// Single pass: O(n) time, O(1) space
+
+class Solution {
+public:
+    int shortestDistance(vector<string>& words, string word1, string word2) {
+        int pos1 = -1, pos2 = -1;
+        int res = words.size();
+        
+        for (int i = 0; i < words.size(); i++) {
+            if (words[i] == word1) {
+                pos1 = i;
+            } else if (words[i] == word2) {
+                pos2 = i; 
+            }
+            
+            if (pos1 != -1 && pos2 != -1) {
+                res = min(res, abs(pos1 - pos2));
+            }
+        }
+        return res;
+    }
+};
