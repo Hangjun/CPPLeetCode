497bb6abd3bee97aa784d1719d978454c2614f02
Hangjun <ajxuhangjun2007@gmail.com>
2017-02-12 20:42:41 -0800

    Update 236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp

--- 236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
+++ 236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
@@ -62,3 +62,29 @@ public:
         tmpPath.pop_back();
     }
 };
+
+/* Remark:
+The above solution traverses the paths twice, and has to store the path. Instead of going from top-dowm, we traverse from the bottom, and once we reach a node which matches one of the two nodes, we pass it up to its parent. The parent would then test its left and right subtree if each contain one of the two nodes. If yes, then the parent must be the LCA and we pass its parent up to the root. If not, we pass the lower node which contains either one of the two nodes (if the left or right subtree contains either p or q), or NULL (if both the left and right subtree does not contain either p or q) up.
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || !p || !q) return NULL;
+        if (root == p || root == q) return root;
+        TreeNode *leftLCA = lowestCommonAncestor(root->left, p, q);
+        TreeNode *rightLCA = lowestCommonAncestor(root->right, p, q);
+        
+        if (leftLCA && rightLCA) return root;
+        return leftLCA ? leftLCA : rightLCA;
+    }
+};
+
