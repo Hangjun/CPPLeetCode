6268d1d7a446c844b90e3cc4588cf9bceb14c1dd
Hangjun <ajxuhangjun2007@gmail.com>
2017-02-14 01:10:09 -0800

    Update 501. Find Mode in Binary Search Tree.cpp

--- 501. Find Mode in Binary Search Tree.cpp
+++ 501. Find Mode in Binary Search Tree.cpp
@@ -20,3 +20,39 @@ Note: If a tree has more than one mode, you can return them in any order.
 Follow up: Could you do that without using any extra space? (Assume that the implicit stack space incurred due to recursion does not count).
 */
 
+// Time O(n), Space O(n). DFS to find frequency counts of all node values.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> findMode(TreeNode* root) {
+        vector<int> res;
+        if (!root) return res;
+        unordered_map<int, int> map; // node value -> frequency count
+        int modeCount = findModeCountDFS(root, map);
+        
+        for (auto p : map) {
+            if (p.second == modeCount) res.push_back(p.first);
+        }
+        
+        return res;
+    }
+    
+    int findModeCountDFS(TreeNode *curNode, unordered_map<int, int> &map) {
+        if (!curNode) return 0;
+        if (map.find(curNode->val) == map.end()) {
+            map[curNode->val] = 1;
+        } else {
+            map[curNode->val]++;
+        }
+        
+        return max(map[curNode->val], max(findModeCountDFS(curNode->left, map), findModeCountDFS(curNode->right, map)));
+    } 
+};
