ff241140caf632e4b3c3b859e53bc30c303edc0a
Hangjun <ajxuhangjun2007@gmail.com>
2017-02-14 02:58:00 -0800

    Update 501. Find Mode in Binary Search Tree.cpp

--- 501. Find Mode in Binary Search Tree.cpp
+++ 501. Find Mode in Binary Search Tree.cpp
@@ -58,4 +58,49 @@ public:
 };
 
 
-// The above solution uses O(n) space. Can we use O(1) space? The answer to this question is Morris traversal.
+/* The above solution uses O(n) space. Can we use O(1) space except the space used in recursion? The idea is to inorder traverse the tree while keeping track of the longest mode values.
+
+We can use Morris traversal to reduce the space complexity to absolutely O(1), but that does not bring any incremental algorithmic beauty to the problem.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> findMode(TreeNode* root) {
+        vector<int> res;
+        if (!root) return res;
+        int curCount = 0, maxCount = 0, curVal = 0;
+        findModeRecur(root, curVal, curCount, maxCount, res);
+        return res;
+    }
+    
+    void findModeRecur(TreeNode *curNode, int &curVal, int &curCount, int &maxCount, vector<int> &res) {
+        if (!curNode) return;
+        findModeRecur(curNode->left, curVal, curCount, maxCount, res);
+        
+        if (!curCount || curVal != curNode->val) {
+            curVal = curNode->val;
+            curCount = 1;
+        } else {
+            curCount++;
+        }
+        
+        if (curCount > maxCount) {
+            maxCount = curCount;
+            res.clear();
+            res.push_back(curVal);
+        } else if (curCount == maxCount) {
+            res.push_back(curVal);
+        }
+        
+        findModeRecur(curNode->right, curVal, curCount, maxCount, res);
+    }
+};
