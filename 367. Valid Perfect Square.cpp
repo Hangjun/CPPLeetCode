c17e655397a4d88e66ce3441cef63f1978956563
Hangjun <ajxuhangjun2007@gmail.com>
2017-02-15 00:09:53 -0800

    Update 367. Valid Perfect Square.cpp

--- 367. Valid Perfect Square.cpp
+++ 367. Valid Perfect Square.cpp
@@ -12,3 +12,42 @@ Example 2:
 Input: 14
 Returns: False
 */
+
+/* The first idea is that a number is a perfect square if and only if it can be expressed as 1+3+5+7+9+....
+The time complexity of this algorithm is O(sqrt(n)).
+*/
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int i = 1;
+        while (num > 0) {
+            num -= i;
+            i += 2;
+        }
+        return num == 0;
+    }
+};
+
+/*
+Another solution is the reverse of the sqrt(x) problem and use binary search. Time = O(log n).
+*/
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int left = 1, right = num;
+        while (left <= right) {
+            long mid = left + (right - left)/2;
+            long long midSquare = mid * mid;
+            if (midSquare == num) {
+                return true;
+            } else if (midSquare < num) {
+                left = (int) mid + 1;
+            } else {
+                right = (int) mid - 1;
+            }
+        }
+        return false;
+    }
+};
+
+// We can of course use Newton's method, but that's not what this problem is really asking for.
