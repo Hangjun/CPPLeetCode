e2216ce0f8b93e3529dcac4f9e188adeb4d89759
Hangjun <ajxuhangjun2007@gmail.com>
2017-02-16 01:16:12 -0800

    Create 128. Longest Consecutive Sequence.cpp

---
+++ 128. Longest Consecutive Sequence.cpp
@@ -0,0 +1,49 @@
+/*
+Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+
+For example,
+Given [100, 4, 200, 1, 3, 2],
+The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
+
+Your algorithm should run in O(n) complexity.
+*/
+
+/*
+The first immediate idea is to sort it first. However, the time complexity is required to be O(n), so sorting is out. The next immediate idea is then to use a hash set to record all the elements in the array. After that, for each element n in the array, we check in the hash set to see if n-1 and n+1 are there, and if yes n-2 and n+2, etc. 
+
+One catch here is that, suppose n+1 indeed exists after checking, we need to remove it from the hash set to avoid duplicated counting (e.g. when the current value is n-1). Each insert/erase operation for a hash set is amorized O(1).
+*/
+
+// Time O(n), Space O(n).
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        unordered_set<int> s;
+        int maxLen = 1;
+        for (auto n : nums) s.insert(n);
+        
+        for (auto n : nums) {
+            if (s.empty()) break;
+            int curNum = n;
+            int curLen = 0;
+            // search n, n+1, n+2,...
+            while (s.count(curNum)) {
+                s.erase(curNum); // avoid duplicated counting
+                curLen++;
+                curNum++;
+            }
+            
+            curNum = n-1;
+            while (s.count(curNum)) {
+                s.erase(curNum); // avoid duplicated counting
+                curLen++;
+                curNum--;
+            }
+            
+            maxLen = max(maxLen, curLen);
+        }
+        
+        return maxLen;
+    }
+};
