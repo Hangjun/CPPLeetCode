423020612d748b47b10b7f09187816dfdca8b1af
Hangjun <ajxuhangjun2007@gmail.com>
2017-02-16 22:15:05 -0800

    Create 449. Serialize and Deserialize BST.cpp

---
+++ 449. Serialize and Deserialize BST.cpp
@@ -0,0 +1,64 @@
+/*
+Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary search tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+The encoded string should be as compact as possible.
+
+Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should be stateless.
+*/
+
+/* The solution of the problem 297. Serialize and Deserialize Binary Tree (https://leetcode.com/problems/serialize-and-deserialize-binary-tree/?tab=Description) can be directly appied here. We recursively DFS traverse the tree, and store the node values in an ostringstream. When deserialize the string, we use istringstream.
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        ostringstream out;
+        serializePrivate(root, out);
+        return out.str();
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        istringstream in(data);
+        return deserializePrivate(in);
+    }
+
+void serializePrivate(TreeNode *root, ostringstream &out) {
+         if (root) {
+             out << root->val << " ";
+             serializePrivate(root->left, out);
+             serializePrivate(root->right, out);
+         } else {
+             out << "# ";
+         }
+     }
+     
+     TreeNode *deserializePrivate(istringstream &in) {
+        string val;
+        in >> val;
+        if (val == "#") return NULL;
+        TreeNode *root = new TreeNode(stoi(val));
+        root->left = deserializePrivate(in);
+        root->right = deserializePrivate(in);
+        
+        return root;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.deserialize(codec.serialize(root));
+
+
