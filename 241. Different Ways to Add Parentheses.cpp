157578b3871e16714b2028a4d0a6aab6505ae7c3
Hangjun <ajxuhangjun2007@gmail.com>
2017-02-19 23:52:58 -0800

    Update 241. Different Ways to Add Parentheses.cpp

--- 241. Different Ways to Add Parentheses.cpp
+++ 241. Different Ways to Add Parentheses.cpp
@@ -21,6 +21,12 @@ Input: "2*3-4*5"
 Output: [-34, -14, -10, -10, 10]
 */
 
+/* 
+The key to solving this problem is to note that, to generate all possible ways to add parentheses is equivalent to evaluate every operation in its highest priority. The leads naturally to a recursive solution. 
+
+Time: O(3^n), Space O(3^n).
+*/
+
 class Solution {
 public:
     vector<int> diffWaysToCompute(string input) {
@@ -37,4 +43,31 @@ public:
         }
         return res.size() ? res : vector<int>{stoi(input)};
     }
+};
+
+/*
+One way to improve the complexity is to memorize all previously computed results using a hash table.
+*/
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string input) {
+        if (ht.find(input) != ht.end()) return ht[input];
+        vector<int> res;
+        for (int i = 0; i < input.size(); i++) {
+            char c = input[i];
+            if (c == '+' || c == '-' || c == '*') {
+                for (int a : diffWaysToCompute(input.substr(0, i))) {
+                    for (int b : diffWaysToCompute(input.substr(i+1))) {
+                        res.push_back(c == '+' ? a+b : c == '-' ? a-b : a*b);
+                    }
+                }
+            }
+        }
+        if (res.empty()) return vector<int>{stoi(input)};
+        ht[input] = res;
+        return res;
+    }
+
+private:
+    unordered_map<string, vector<int>> ht;
 };
