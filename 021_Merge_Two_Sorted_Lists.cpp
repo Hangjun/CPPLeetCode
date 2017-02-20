be0bff4e399b112b63dc9dbae2debf2978bb6b26
Hangjun <ajxuhangjun2007@gmail.com>
2017-02-20 01:28:14 -0800

    Update 021_Merge_Two_Sorted_Lists.cpp

--- 021_Merge_Two_Sorted_Lists.cpp
+++ 021_Merge_Two_Sorted_Lists.cpp
@@ -38,3 +38,38 @@ public:
         return dummyNode->next;
     }
 };
+
+/*
+This problem explicitly requires the merged list to be a newly created list. However, if we can modify the lists in-place, here is its implementation:
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *dummyNode = new ListNode(-1);
+        ListNode *prevNode = dummyNode;
+        prevNode->next = l1;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                l1 = l1->next;
+            } else {
+                ListNode *tmp = l2->next;
+                prevNode->next = l2;
+                l2->next = l1;
+                l2 = tmp;
+            }
+            prevNode = prevNode->next;
+        }
+        
+        if (l2) prevNode->next = l2;
+        return dummyNode->next;
+    }
+};
