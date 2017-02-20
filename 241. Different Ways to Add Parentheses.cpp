7a30d9f257bf82a6de24e396fb163630b20842c6
Hangjun <ajxuhangjun2007@gmail.com>
2017-02-19 23:34:53 -0800

    Update 241. Different Ways to Add Parentheses.cpp

--- 241. Different Ways to Add Parentheses.cpp
+++ 241. Different Ways to Add Parentheses.cpp
@@ -20,3 +20,21 @@ Input: "2*3-4*5"
 (((2*3)-4)*5) = 10
 Output: [-34, -14, -10, -10, 10]
 */
+
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string input) {
+        vector<int> res;
+        for (int i = 0; i < input.size(); i++) {
+            char c = input[i];
+            if (c == '+' || c == '-' || c == '*') {
+                for (int a : diffWaysToCompute(input.substr(0, i))) {
+                    for (int b : diffWaysToCompute(input.substr(i+1))) {
+                        res.push_back(c == '+' ? a+b : c == '-' ? a-b : a*b);
+                    }
+                }
+            }
+        }
+        return res.size() ? res : vector<int>{stoi(input)};
+    }
+};
