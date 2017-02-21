f419831a2507cd6d754881018b56222e65a0f2ae
Hangjun <ajxuhangjun2007@gmail.com>
2017-02-20 18:09:27 -0800

    Create 272. Closest Binary Search Tree Value II.cpp

---
+++ 272. Closest Binary Search Tree Value II.cpp
@@ -0,0 +1,30 @@
+/*
+Given a non-empty binary search tree and a target value, find k values in the BST that are closest to the target.
+
+Note:
+Given target value is a floating point.
+You may assume k is always valid, that is: k ≤ total nodes.
+You are guaranteed to have only one unique set of k values in the BST that are closest to the target.
+Follow up:
+Assume that the BST is balanced, could you solve it in less than O(n) runtime (where n = total nodes)?
+
+Hint:
+
+Consider implement these two helper functions:
+getPredecessor(N), which returns the next smaller node to N.
+getSuccessor(N), which returns the next larger node to N.
+Try to assume that each node has a parent pointer, it makes the problem much easier.
+Without parent pointer we just need to keep track of the path from the root to the current node using a stack.
+You would need two stacks to track the path in finding predecessor and successor node separately.
+*/
+
+/*
+The idea is to store the predecessors and successors of the node closest to the target, and then perform a "merge sort" to find, from the predecessors and successors, the remaining closest nodes. 
+
+We can store the predecessors and successors of the closest node in two stacks.
+
+Time: O(logn + k)
+Space: O(n)
+*/
+
+
