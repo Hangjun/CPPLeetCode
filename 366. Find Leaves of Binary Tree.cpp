190f13b834ec024c23dea49864d04035d32329db
Hangjun <ajxuhangjun2007@gmail.com>
2017-02-21 00:57:11 -0800

    Update 366. Find Leaves of Binary Tree.cpp

--- 366. Find Leaves of Binary Tree.cpp
+++ 366. Find Leaves of Binary Tree.cpp
@@ -25,3 +25,33 @@ Explanation:
 Returns [4, 5, 3], [2], [1].
 */
 
+/*
+Grouping together all the nodes at the same level does not work, but computing the height (distance from the node to the deepest leaf) works.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> findLeaves(TreeNode* root) {
+        vector<vector<int>> res;
+        if (!root) return res;
+        computeHeightDFS(root, res);
+        return res;
+    }
+    
+    int computeHeightDFS(TreeNode *curNode, vector<vector<int>> &res) {
+        if (!curNode) return -1; // height of leaf node is 0
+        int height = 1 + max(computeHeightDFS(curNode->left, res), computeHeightDFS(curNode->right, res));
+        if (height == res.size()) res.push_back(vector<int>()); // new height discovered
+        res[height].push_back(curNode->val);
+        return height;
+    }
+};
