f4fabcc8d1bfabf0bfa68b67fb780f6796faca48
Hangjun <ajxuhangjun2007@gmail.com>
2017-02-26 00:40:19 -0800

    Create 167. Two Sum II - Input array is sorted.cpp

---
+++ 167. Two Sum II - Input array is sorted.cpp
@@ -0,0 +1,30 @@
+/*
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> res;
+        if (numbers.empty()) return res;
+        int left = 0, right = numbers.size()-1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                res.push_back(left+1);
+                res.push_back(right+1);
+                return res;
+            } else if (numbers[left] + numbers[right] < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+};
