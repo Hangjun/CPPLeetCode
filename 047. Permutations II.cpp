2916a5d0bf0aac25d95d38dfb4154afb55c83365
Hangjun <ajxuhangjun2007@gmail.com>
2017-02-25 19:00:44 -0800

    Update 047. Permutations II.cpp

--- 047. Permutations II.cpp
+++ 047. Permutations II.cpp
@@ -17,7 +17,7 @@ public:
         if (nums.empty()) return res;
         vector<int> curSoln;
         vector<bool> canUse(nums.size(), true);
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end()); // when in backtracking, SORT!!!
         permuteDFS(nums, 0, canUse, curSoln, res);
         return res;
     }
@@ -28,14 +28,12 @@ public:
             return;
         }
         for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i-1] && canUse[i-1]) continue;
-            if (canUse[i] == true) {
-                canUse[i] = false;
-                curSoln.push_back(nums[i]);
-                permuteDFS(nums, start+1, canUse, curSoln, res);
-                canUse[i] = true;
-                curSoln.pop_back();
-            }
+            if ((i > 0 && nums[i] == nums[i-1] && canUse[i-1]) || !canUse[i]) continue;
+            canUse[i] = false;
+            curSoln.push_back(nums[i]);
+            permuteDFS(nums, start+1, canUse, curSoln, res);
+            canUse[i] = true;
+            curSoln.pop_back();
         }
     }
 };
