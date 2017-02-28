144130a377bf1a5a2d293bf3c40473d6ca86b4df
Hangjun <ajxuhangjun2007@gmail.com>
2017-02-27 23:36:40 -0800

    Update 198_House_Robber.cpp

--- 198_House_Robber.cpp
+++ 198_House_Robber.cpp
@@ -28,12 +28,32 @@ public:
         if (nums.empty()) return 0;
         int n = nums.size();
         if (n == 1) return nums[0];
-        vector<int> dp(3, 0); // dp[i] = amount of money up to the ith house such that the ith house must be robbed
+        vector<int> dp(2, 0); // dp[i] = amount of money up to the ith house such that the ith house must be robbed
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
         for (int i = 2; i < n; i++) {
-            dp[i%3] = max(dp[(i-2)%3] + nums[i], dp[(i-1)%3]);
+            dp[i%2] = max(dp[(i-2)%2] + nums[i], dp[(i-1)%2]);
         }
-        return dp[(n-1)%3];
+        return dp[(n-1)%2];
+    }
+};
+
+// More directly:
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int n = nums.size();
+        int a = 0, b = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i % 2) {
+                a += nums[i];
+                a = max(a, b);
+            } else {
+                b += nums[i];
+                b = max(a, b);
+            }
+        }
+        return max(a, b);
     }
 };
