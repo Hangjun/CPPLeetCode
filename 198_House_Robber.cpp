bf7ccf668d1a1f4b87f9fe5b29558a6a222be7eb
Hangjun <ajxuhangjun2007@gmail.com>
2017-02-27 23:27:34 -0800

    Update 198_House_Robber.cpp

--- 198_House_Robber.cpp
+++ 198_House_Robber.cpp
@@ -4,3 +4,36 @@ You are a professional robber planning to rob houses along a street. Each house 
 Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
 */
 
+// Time: O(n), Space: O(n)
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        vector<int> dp(n, 0); // dp[i] = amount of money up to the ith house such that the ith house must be robbed
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
+        }
+        return dp[n-1];
+    }
+};
+
+// We can improve the space complexity to O(1) using the rolling array trick:
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        vector<int> dp(3, 0); // dp[i] = amount of money up to the ith house such that the ith house must be robbed
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i%3] = max(dp[(i-2)%3] + nums[i], dp[(i-1)%3]);
+        }
+        return dp[(n-1)%3];
+    }
+};
