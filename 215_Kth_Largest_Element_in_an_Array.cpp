b875a359ab2f876e1ac8933191dec4e187dce46f
Hangjun <ajxuhangjun2007@gmail.com>
2017-03-01 21:19:31 -0800

    Update 215_Kth_Largest_Element_in_an_Array.cpp

--- 215_Kth_Largest_Element_in_an_Array.cpp
+++ 215_Kth_Largest_Element_in_an_Array.cpp
@@ -8,6 +8,7 @@ Note:
 You may assume k is always valid, 1 ≤ k ≤ array's length.
 */
 
+// Time: O(nlogn), Space: O(n)
 class myCompare {
 public: 
     bool operator()(int &a, int &b) {
@@ -29,5 +30,32 @@ public:
         }
         
         return maxHeap.top();
+    }
+};
+
+// Quick select: Time: O(n) average, O(n^2) worse case, Space: O(1). T(n) = T(n/2) + O(n).
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int left = 0, right = nums.size()-1;
+        while (true) {
+            int pos = partition(nums, left, right);
+            if (pos == k-1) return nums[pos];
+            else if (pos > k-1) right = pos-1;
+            else left = pos+1;
+        }
+    }
+private:
+    int partition(vector<int> &nums, int left, int right) {
+        // partition nums such that [>= pivot, pivot, <= pivot] since we are looking for the kth largest
+        int pivot = nums[left];
+        int l = left+1, r = right;
+        while (l <= r) {
+            if (nums[l] < pivot && nums[r] > pivot) swap(nums[l++], nums[r--]);
+            if (nums[l] >= pivot) l++;
+            if (nums[r] <= pivot) r--;
+        }
+        swap(nums[left], nums[r]);
+        return r;
     }
 };
