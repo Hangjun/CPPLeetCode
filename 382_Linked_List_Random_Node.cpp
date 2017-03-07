a47e324bc29e8b8037da762f30d70cb3904761c0
Hangjun <ajxuhangjun2007@gmail.com>
2017-03-07 01:14:48 -0800

    Update 382_Linked_List_Random_Node.cpp

--- 382_Linked_List_Random_Node.cpp
+++ 382_Linked_List_Random_Node.cpp
@@ -38,7 +38,7 @@ public:
         int count = 2;
         ListNode *curNode = head->next;
         while (curNode) {
-            if (rand() % count == 1) res = curNode->val;
+            if (rand() % count == 0) res = curNode->val;
             curNode = curNode->next;
             count++;
         }
