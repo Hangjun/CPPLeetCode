eb27884f420382c8a8d45fe924816d28573dc100
Hangjun <ajxuhangjun2007@gmail.com>
2017-03-08 01:34:26 -0800

    Update 027. Remove Element.cpp

--- 027. Remove Element.cpp
+++ 027. Remove Element.cpp
@@ -16,3 +16,21 @@ Try two pointers.
 Did you use the property of "the order of elements can be changed"?
 What happens when the elements to remove are rare?
 */
+
+/*
+This problem is exactly the same as Problem 26 (https://leetcode.com/problems/remove-duplicates-from-sorted-array/?tab=Description). See my solution here: https://github.com/Hangjun/MyLeetCode/blob/master/026.%20Remove%20Duplicates%20from%20Sorted%20Array.cpp.
+
+Time: O(n), Space: O(1).
+*/
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if (nums.empty()) return 0;
+        int len = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) nums[++len] = nums[i];
+        }
+        return len+1;
+    }
+};
+
