3cef3f25d0b52029185c503c7177a54e2bcbc34c
Hangjun <ajxuhangjun2007@gmail.com>
2017-03-08 01:16:51 -0800

    Update 345. Reverse Vowels of a String.cpp

--- 345. Reverse Vowels of a String.cpp
+++ 345. Reverse Vowels of a String.cpp
@@ -11,3 +11,25 @@ Note:
 The vowels does not include the letter "y".
 */
 
+// Time: O(n), Space: O(1).
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int i = 0, j = s.size();
+        while (i < j) {
+            if (!isVowel(s[i])) {
+                i++;
+            } else if (!isVowel(s[j])) {
+                j--;
+            } else {
+                swap(s[i++], s[j--]);
+            }
+        }
+        return s;
+    }
+    
+    bool isVowel(char c) {
+        char cLower = tolower(c);
+        return cLower == 'a' || cLower == 'e' || cLower == 'i' || cLower == 'o' || cLower == 'u';
+    }
+};
