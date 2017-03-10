240d0e620d89a69f0dbb1aa453bc432b17285029
Hangjun <ajxuhangjun2007@gmail.com>
2017-03-09 21:42:40 -0800

    Create 350. Intersection of Two Arrays II.cpp

---
+++ 350. Intersection of Two Arrays II.cpp
@@ -0,0 +1,54 @@
+/*
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
+
+Note:
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+Follow up:
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+*/
+
+// Time: O(m+n), Space: O(min(m, n)). Hash table.
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        unordered_map<int, int> ht;
+        for (int n : nums1) ht[n]++;
+        for (int n : nums2) {
+            if (ht.find(n) != ht.end()) {
+                res.push_back(n);
+                if (--ht[n] == 0) ht.erase(n);
+            }
+        }
+        return res;
+    }
+};
+
+// Sorting: Time: O(min(m, n) * log(min(m, n))), Space: O(1).
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int i = 0, j = 0;
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) {
+                res.push_back(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return res;
+    }
+};
