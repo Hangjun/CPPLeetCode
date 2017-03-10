4be932d96753fb8efa553506c205dd6e49e29fc4
Hangjun <ajxuhangjun2007@gmail.com>
2017-03-09 22:48:40 -0800

    Update 278_First_Bad_Version.cpp

--- 278_First_Bad_Version.cpp
+++ 278_First_Bad_Version.cpp
@@ -10,7 +10,6 @@ You are given an API bool isBadVersion(version) which will return whether versio
 Time: O(logn)
 Space: O(1)
 */
-
 // Forward declaration of isBadVersion API.
 bool isBadVersion(int version);
 
@@ -18,12 +17,10 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int left = 1, right = n;
-        while (left <= right) {
-            int mid = left + (right - left)/2;
-            if (isBadVersion(mid))
-                right = mid-1;
-            else
-                left = mid+1;
+        while (left < right) {
+            int mid = left + (right-left)/2;
+            if (!isBadVersion(mid)) left = mid+1;
+            else right = mid;
         }
         return left;
     }
