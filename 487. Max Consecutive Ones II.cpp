cad93bf7b562cd39f4ae1fc22c48478c740a18e2
Hangjun <ajxuhangjun2007@gmail.com>
2017-03-10 23:59:57 -0800

    Update 487. Max Consecutive Ones II.cpp

--- 487. Max Consecutive Ones II.cpp
+++ 487. Max Consecutive Ones II.cpp
@@ -14,3 +14,21 @@ Follow up:
 What if the input numbers come in one by one as an infinite stream? In other words, you can't store all numbers coming from the stream as it's too large to hold in memory. Could you solve it efficiently?
 */
 
+// Time: O(n), Space: O(1). Two pointer sliding window solution. Works for any k: = number of zeros you can flip.
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int res = 0;
+        int zeros = 0;
+        int k = 1;
+        for (int left = 0, right = 0; right < nums.size(); right++) {
+            if (nums[right] == 0) zeros++;
+            while (zeros > k) {
+                if (nums[left++] == 0) zeros--;
+            }
+            res = max(res, right-left+1);
+        }
+        return res;
+    }
+};
