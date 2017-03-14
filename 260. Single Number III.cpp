1aa64273597b27529a525c9a22b3dc03d41d498b
Hangjun <ajxuhangjun2007@gmail.com>
2017-03-13 21:32:52 -0700

    Create 260. Single Number III.cpp

---
+++ 260. Single Number III.cpp
@@ -0,0 +1,46 @@
+/*
+Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+
+For example:
+
+Given nums = [1, 2, 1, 3, 2, 5], return [3, 5].
+
+Note:
+The order of the result is not important. So in the above example, [5, 3] is also correct.
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
+*/
+
+/*
+Analysis: We first xor all elements in the array. The resulting number is the xor of the two single numbers. To separate them, we find the first index of bit 1 in the previous result (the first bit that the two single numbers differ). Then we scan the array one more time such that for numbers that have that bit equal to 1, we xor it into one number, and for other numbers we xor them into another number. The resulting two numbers are the two single numbers we are looking for.
+
+Time: O(n), Space: O(1). 2 Passes. 2n+2 problem.
+*/
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int res = 0;
+        for (int n : nums) res ^= n;
+        
+        int res1 = 0, res2 = 0; // res1 xor res2 = res
+        int index = firstOneBitIndex(res);
+        for (int n : nums) {
+            if (equalOneBit(n, index)) res1 ^= n;
+            else res2 ^= n;
+        }
+        return vector<int>{res1, res2};
+        
+    }
+    
+    int firstOneBitIndex(int n) {
+        int index = 0;
+        while ((n & 1) == 0) {
+            ++index;
+            n >>= 1;
+        }
+        return index;
+    }
+    
+    bool equalOneBit(int n, int index) {
+        return (n >>= index) & 1;
+    }
+};
