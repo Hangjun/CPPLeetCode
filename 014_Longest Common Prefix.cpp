669efda335a7cebd4a2632ea229f8ee8b1c7c937
Hangjun <ajxuhangjun2007@gmail.com>
2017-03-21 13:24:31 -0700

    Update 014_Longest Common Prefix.cpp

--- 014_Longest Common Prefix.cpp
+++ 014_Longest Common Prefix.cpp
@@ -1,24 +1,28 @@
+/*
 Write a function to find the longest common prefix string amongst an array of strings. 
+*/
 
+/*
+Analysis: The first idea is to compute the minimum length of all the strings first, and then traverse the strs to compute the lcp one character at a time. Let n be the common minimum length, and m be the total number of strings. Then the algorithm takes O(m*n) time. We can also do this constructively and find the lcp with 2 strings at a time.
+*/
 
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
         int n = strs.size();
-        if (n == 0)  return "";
-        if (n == 1)  return strs[0];
         string lcp = strs[0];
-        for (int i = 1; i < strs.size(); i++) {
-            lcp = commonSubstr(lcp, strs[i]);
+        for (int i = 1; i < n; i++) {
+            findLCP(lcp, strs[i]);
         }
         return lcp;
     }
     
-    string commonSubstr(string s1, string s2){
-        int n = min(s1.size(), s2.size());
-        for (int i = 0; i < n; i++) {
-            if (s1[i] != s2[i])  return s1.substr(0, i);
-        }
-        return s1.substr(0, n);
+    void findLCP(string &lcp, string s) {
+        int n = min(lcp.size(), s.size());
+        int i = 0;
+        while (i < n && lcp[i] == s[i]) i++;
+
+        lcp = lcp.substr(0, i);
     }
 };
