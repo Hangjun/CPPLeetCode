5a73bfe7731b3aba304d0fdaac77f14a9dc573de
Hangjun <ajxuhangjun2007@gmail.com>
2017-03-20 23:45:23 -0700

    Create 441. Arranging Coins.cpp

---
+++ 441. Arranging Coins.cpp
@@ -0,0 +1,44 @@
+/*
+You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+
+Given n, find the total number of full staircase rows that can be formed.
+
+n is a non-negative integer and fits within the range of a 32-bit signed integer.
+
+Example 1:
+
+n = 5
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤
+
+Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+n = 8
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤ ¤
+¤ ¤
+
+Because the 4th row is incomplete, we return 3.
+*/
+
+// Binary Search. Time: O(logn), Space: O(1).
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        if (n == 0) return 0;
+        int left = 1, right = n;
+        while (left <= right) {
+            long mid = left + (right-left)/2;
+            if ((1+mid) * mid / 2 <= n) left = mid+1;
+            else right = mid-1;
+        }
+        return left-1;
+    }
+};
