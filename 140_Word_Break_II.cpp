7190b376fbada22a478a584f0a919780aabb79e1
Hangjun <ajxuhangjun2007@gmail.com>
2017-03-23 22:34:20 -0700

    Update 140_Word_Break_II.cpp

--- 140_Word_Break_II.cpp
+++ 140_Word_Break_II.cpp
@@ -8,7 +8,7 @@ dict = ["cat", "cats", "and", "sand", "dog"].
 A solution is ["cats and dog", "cat sand dog"]. 
 */
 
-The first idea is to use DFS to collect all the local and global paths:
+// The first idea is to use DFS to collect all the local and global paths:
 
 class Solution {
 public:
@@ -44,4 +44,38 @@ public:
     }
 };
 
-However, this is reported Time Limit Exceeded by LeetCode OJ. 
+/*
+However, this solution exceeds time limit. We then use memorization to record, for each substring, all of its breakable subtring solutions.
+*/
+class Solution {
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        return wordBreakDFS(s, dict);
+    }
+    
+    vector<string> wordBreakDFS(string s,  unordered_set<string> &dict) {
+        if (ht.find(s) != ht.end()) return ht[s];
+        vector<string> res;
+        if (dict.count(s)) res.push_back(s);
+        
+        for (int i = 1; i < s.size(); i++) {
+            string curBreak = s.substr(0, i);
+            if (!dict.count(curBreak)) continue;
+            string remaining = s.substr(i);
+            vector<string> prevBreak = combine(curBreak, wordBreakDFS(remaining, dict));
+            res.insert(res.end(), prevBreak.begin(), prevBreak.end());
+        }
+        
+        ht[s] = res;
+        return res;
+    }
+
+private:
+    unordered_map<string, vector<string>> ht; // current string -> breakable substrings
+    
+    vector<string> combine(string word, vector<string> strs) {
+        for (int i = 0; i < strs.size(); i++) strs[i] = word + " " + strs[i];
+        return strs;
+    }
+};
