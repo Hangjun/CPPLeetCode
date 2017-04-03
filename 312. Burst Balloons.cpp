a826f84cbfa74dab80d03cc6afdda298873f9720
Hangjun <ajxuhangjun2007@gmail.com>
2017-04-02 17:15:23 -0700

    Update 312. Burst Balloons.cpp

--- 312. Burst Balloons.cpp
+++ 312. Burst Balloons.cpp
@@ -18,3 +18,60 @@ Return 167
 
 */
 
+/*
+Analysis: Be Naive First
+
+When I first get this problem, it is far from dynamic programming to me. I started with the most naive idea the backtracking.
+
+We have n balloons to burst, which mean we have n steps in the game. In the i th step we have n-i balloons to burst, i = 0~n-1. Therefore we are looking at an algorithm of O(n!). Well, it is slow, probably works for n < 12 only.
+
+Of course this is not the point to implement it. We need to identify the redundant works we did in it and try to optimize.
+
+Well, we can find that for any balloons left the maxCoins does not depends on the balloons already bursted. This indicate that we can use memorization (top down) or dynamic programming (bottom up) for all the cases from small numbers of balloon until n balloons. How many cases are there? For k balloons there are C(n, k) cases and for each case it need to scan the k balloons to compare. The sum is quite big still. It is better than O(n!) but worse than O(2^n).
+
+Better idea
+
+We then think can we apply the divide and conquer technique? After all there seems to be many self similar sub problems from the previous analysis.
+
+Well, the nature way to divide the problem is burst one balloon and separate the balloons into 2 sub sections one on the left and one one the right. However, in this problem the left and right become adjacent and have effects on the maxCoins in the future.
+
+Then another interesting idea come up. Which is quite often seen in dp problem analysis. That is reverse thinking. Like I said the coins you get for a balloon does not depend on the balloons already burst. Therefore
+instead of divide the problem by the first balloon to burst, we divide the problem by the last balloon to burst.
+
+Why is that? Because only the first and last balloons we are sure of their adjacent balloons before hand!
+
+For the first we have nums[i-1]*nums[i]*nums[i+1] for the last we have nums[-1]*nums[i]*nums[n].
+
+OK. Think about n balloons if i is the last one to burst, what now?
+
+We can see that the balloons is again separated into 2 sections. But this time since the balloon i is the last balloon of all to burst, the left and right section now has well defined boundary and do not affect each other! Therefore we can do either recursive method with memoization or dp.
+
+Final
+
+Here comes the final solutions. Note that we put 2 balloons with 1 as boundaries and also burst all the zero balloons in the first round since they won't give any coins.
+The algorithm runs in O(n^3) which can be easily seen from the 3 loops in dp solution.
+
+Time: O(n^3), Space: O(n^2).
+*/
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));  // dp[i][j] = max coins generated from interval [i, j] inclusive
+        
+        for (int len = 1; len <= n; len++) { // all possible sliding window length
+            for (int left = 1; left <= n-len+1; left++) {
+                int right = left+len-1;
+                // index of last balloon to burst
+                for (int k = left; k <= right; k++) {
+                    dp[left][right] = max(dp[left][right], nums[left-1]*nums[k]*nums[right+1] + dp[left][k-1] + dp[k+1][right]);
+                }
+            }
+        }
+        
+        return dp[1][n];
+    }
+};
