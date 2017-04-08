2e415b529098628eb8d04119a277114aab84bdd3
Hangjun <ajxuhangjun2007@gmail.com>
2017-04-08 15:04:06 -0700

    Update 047. Permutations II.cpp

--- 047. Permutations II.cpp
+++ 047. Permutations II.cpp
@@ -15,25 +15,26 @@ public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> res;
         if (nums.empty()) return res;
-        vector<int> curSoln;
+        vector<int> localSoln;
         vector<bool> canUse(nums.size(), true);
-        sort(nums.begin(), nums.end()); // when in backtracking, SORT!!!
-        permuteDFS(nums, 0, canUse, curSoln, res);
+        sort(nums.begin(), nums.end()); // When in backtracking, SORT!!!
+        dfs(nums, canUse, localSoln, res);
         return res;
     }
     
-    void permuteDFS(vector<int> &nums, int start, vector<bool> &canUse, vector<int> &curSoln, vector<vector<int>> &res) {
-        if (start == nums.size()) {
-            res.push_back(curSoln);
+    void dfs(vector<int> &nums, vector<bool> &canUse, vector<int> &localSoln, vector<vector<int>> &res) {
+        if (localSoln.size() == nums.size()) {
+            res.push_back(localSoln);
             return;
         }
+        
         for (int i = 0; i < nums.size(); i++) {
             if ((i > 0 && nums[i] == nums[i-1] && canUse[i-1]) || !canUse[i]) continue;
             canUse[i] = false;
-            curSoln.push_back(nums[i]);
-            permuteDFS(nums, start+1, canUse, curSoln, res);
+            localSoln.push_back(nums[i]);
+            dfs(nums, canUse, localSoln, res);
+            localSoln.pop_back();
             canUse[i] = true;
-            curSoln.pop_back();
         }
     }
 };
