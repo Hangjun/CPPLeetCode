61598f8a2718623580ae23ee3fb4d76a9b327374
Hangjun <ajxuhangjun2007@gmail.com>
2017-04-08 19:02:57 -0700

    Update 421. Maximum XOR of Two Numbers in an Array.cpp

--- 421. Maximum XOR of Two Numbers in an Array.cpp
+++ 421. Maximum XOR of Two Numbers in an Array.cpp
@@ -81,3 +81,88 @@ public:
         return T.helper(nums);
     }
 };
+
+/*
+But we are not done yet. Consider the follow up question:
+
+Given an array of integers. find the maximum XOR subarray value in given array. Expected time complexity O(n).
+
+Trie is still the data structure to use, but we need to store additional information into the Trie. The key is to observe that, if f(i,j) is xor subrray value from i to j, then f(i, j) = f(0, j) xor f(0, i-1). Make sure you understand why this is true.
+*/
+// C++ program for a Trie based O(n) solution to find max subarray XOR
+class TrieNode
+{
+public:
+	int val;
+	TrieNode *ch[2];
+	
+	TrieNode() :val(0){
+	    ch[0] = NULL;
+	    ch[1] = NULL;
+	}
+};
+
+class Trie {
+public:
+    Trie() { root = new TrieNode(); }
+    
+    // Inserts pre_xor (xor of nums[0...i]) to trie
+    void insert(int pre_xor)
+    {
+	    TrieNode *curNode = root;
+
+	    // Start from the msb, insert all bits of pre_xor into Trie
+	    for (int i=31; i>=0; i--)
+	    {
+		    // Find current bit in given prefix
+		    bool index = pre_xor & (1<<i);
+
+		    // Create a new node if needed
+		    if (!curNode->ch[index]) curNode->ch[index] = new TrieNode();
+            curNode = curNode->ch[index];
+	    }
+	    // Store value at leaf node
+	    curNode->val = pre_xor;
+    }
+    
+    int query(int pre_xor) {
+        TrieNode *curNode = root;
+        for (int i = 31; i >= 0; i--) {
+            int index = pre_xor & (1 << i);
+            if (curNode->ch[index ^ 1]) {
+                curNode = curNode->ch[index ^ 1];
+            } else {
+                curNode = curNode->ch[index];
+            }
+        }
+        return pre_xor ^ curNode->val;
+    }
+    
+private:
+    TrieNode *root;
+};
+
+// Returns maximum XOR value of a subarray in nums[0..n-1]
+int maxSubarrayXOR(vector<int> &nums) {
+    int res = INT_MIN;
+    int pre_xor = 0;
+    Trie T;
+    T.insert(0);
+        
+    for (int n : nums) {
+        pre_xor ^= n;
+        T.insert(pre_xor);
+        res = max(res, T.query(pre_xor));
+    }
+    
+    return res;
+}
+
+// Driver program to test above functions
+int main()
+{
+    
+	vector<int> nums = {8, 1, 2, 12};
+	cout << "Max subarray XOR is " << maxSubarrayXOR(nums);
+	return 0;
+}
