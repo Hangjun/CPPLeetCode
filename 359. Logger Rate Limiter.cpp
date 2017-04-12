d2e72668b4393d24230ffff3a8c6b70900f4b9de
Hangjun <ajxuhangjun2007@gmail.com>
2017-04-11 22:25:50 -0700

    Update 359. Logger Rate Limiter.cpp

--- 359. Logger Rate Limiter.cpp
+++ 359. Logger Rate Limiter.cpp
@@ -28,3 +28,30 @@ logger.shouldPrintMessage(10,"foo"); returns false;
 logger.shouldPrintMessage(11,"foo"); returns true;
 */
 
+class Logger {
+public:
+    /** Initialize your data structure here. */
+    Logger() {
+        
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    bool shouldPrintMessage(int timestamp, string message) {
+        if (ht.find(message) == ht.end() || timestamp - ht[message] >= 10) {
+            ht[message] = timestamp;
+            return true;
+        }
+        return false;
+    }
+
+private:
+    unordered_map<string, int> ht; // message -> timestamp
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * bool param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
