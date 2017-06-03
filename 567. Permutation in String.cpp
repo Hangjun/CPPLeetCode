1c938c854c0e83defba49737a6c69526b8905f81
Hangjun <ajxuhangjun2007@gmail.com>
2017-06-02 23:25:42 -0700

    Update 567. Permutation in String.cpp

--- 567. Permutation in String.cpp
+++ 567. Permutation in String.cpp
@@ -13,3 +13,33 @@ The input strings only contain lower case letters.
 The length of both given strings is in range [1, 10,000].
 */
 
+/*
+Analysis: This is essentially the same problem as Problem 438. Find All Anagrams in a String.
+
+Time: O(n), Space: O(k). Two pointer sliding window.
+*/
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int k = s1.size(), n = s2.size();
+        if (k > n) return false;
+        
+        vector<int> base(256, 0);
+        vector<int> match(256, 0);
+        for (int i = 0; i < k; i++) {
+            ++base[s1[i] - 'a'];
+            ++match[s2[i] - 'a'];
+        }
+        if (base == match) return true;
+        
+        // now we have [0, k-1], we maintain a sliding window of size k: [i-k+1, i]
+        for (int i = k; i < n; i++) {
+            ++match[s2[i] - 'a'];
+            --match[s2[i-k] - 'a'];
+            if (base == match) return true;
+        }
+        
+        return false;
+    }
+};
