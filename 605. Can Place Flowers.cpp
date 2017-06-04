e4d1fa05a6a5db81119c5677effdcfa65e68a120
Hangjun <ajxuhangjun2007@gmail.com>
2017-06-03 23:33:07 -0700

    Update 605. Can Place Flowers.cpp

--- 605. Can Place Flowers.cpp
+++ 605. Can Place Flowers.cpp
@@ -24,12 +24,13 @@ Time: O(n), Space; O(1).
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        if (n == 0) return true;
         for (int i = 0; i < flowerbed.size(); i++) {
             if (!flowerbed[i] && (i == 0 || !flowerbed[i-1]) && (i == flowerbed.size()-1 || !flowerbed[i+1])) {
                 flowerbed[i] = 1;
-                --n;
+                if (--n <= 0) return true; // early termination
             }
         }
-        return n <= 0;
+        return false;
     }
 };
