32b14b55a1f1b18892102f28e7a2ac7dac1e880d
Hangjun <ajxuhangjun2007@gmail.com>
2017-06-08 22:28:23 -0700

    Update 012_Integer_to_Roman.cpp

--- 012_Integer_to_Roman.cpp
+++ 012_Integer_to_Roman.cpp
@@ -21,15 +21,16 @@ public:
         string res;
         string dict[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         int val[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        
         for (int i = 0; i < 13; i++) {
-            if (num >= val[i]) {
-                int count = num / val[i];
-                num %= val[i];
-                for (int j = 0; j < count; j++) {
-                    res.append(dict[i]);
-                }
+            if (num < val[i]) continue;
+            int count = num / val[i];
+            num %= val[i];
+            for (int j = 0; j < count; j++) {
+                res.append(dict[i]);
             }
         }
+        
         return res;
     }
 };
