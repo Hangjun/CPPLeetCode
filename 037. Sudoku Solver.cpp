77ee43d5aac83214ea375c7d68d4d2e6a08c7461
Hangjun <ajxuhangjun2007@gmail.com>
2017-06-21 19:30:07 -0700

    Update 037. Sudoku Solver.cpp

--- 037. Sudoku Solver.cpp
+++ 037. Sudoku Solver.cpp
@@ -12,3 +12,35 @@ A sudoku puzzle...
 ...and its solution numbers marked in red.
 */
 
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        if (board.empty() || board[0].empty()) return;
+        solve(board);
+    }
+    
+    bool solve(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') continue;
+                for (char c = '1'; c <= '9'; c++) {
+                    if (!isValid(board, i, j, c)) continue;
+                    board[i][j] = c;
+                    if (solve(board)) return true;
+                    board[i][j] = '.';
+                }
+                return false;
+            } 
+        }
+        return true;
+    }
+    
+    bool isValid(vector<vector<char>>& board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == c) return false;
+            if (board[row][i] == c) return false;
+            if (board[3 * (row / 3) + i / 3][ 3 * (col / 3) + i % 3] == c) return false;
+        }
+        return true;
+    }
+};
