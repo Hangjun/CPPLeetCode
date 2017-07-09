7e6b9d0dddc16f031d20ab2402e4a914ea8f4912
Hangjun <ajxuhangjun2007@gmail.com>
2017-07-08 22:40:00 -0700

    Update 252. Meeting Rooms.cpp

--- 252. Meeting Rooms.cpp
+++ 252. Meeting Rooms.cpp
@@ -6,3 +6,24 @@ Given [[0, 30],[5, 10],[15, 20]],
 return false.
 */
 
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        if (intervals.empty()) return true;
+        sort(intervals.begin(), intervals.end(), [](Interval i1, Interval i2){return i1.start < i2.start;});
+        for (int i = 0; i < intervals.size()-1; i++) {
+            if (intervals[i].end > intervals[i+1].start) return false;
+        }
+        
+        return true;
+    }
+};
