18c08c98ee7bfe4c08c3ec725bfa9309e80d1608
Hangjun <ajxuhangjun2007@gmail.com>
2017-08-19 14:53:29 -0700

    Update 643. Maximum Average Subarray I.cpp

--- 643. Maximum Average Subarray I.cpp
+++ 643. Maximum Average Subarray I.cpp
@@ -9,3 +9,30 @@ Note:
 1 <= k <= n <= 30,000.
 Elements of the given array will be in the range [-10,000, 10,000].
 */
+
+/*
+Solution: Very straightforward sliding window idea. 
+
+Time: O(n), Space: O(1).
+*/
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        const int n = nums.size();
+        long long sum = 0;
+        // warm up with the first width k window
+        for (int i = 0; i < min(k, n); i++) sum += nums[i];
+        
+        double res = sum / (double) min(k, n);
+        if (n <= k) return res;
+        
+        for (int i = k; i < n; i++) {
+            // kick the first element from sum; recompute average
+            sum -= nums[i-k];
+            sum += nums[i];
+            res = max(res, sum / (double) k);
+        }
+        
+        return res;
+    }
+};
