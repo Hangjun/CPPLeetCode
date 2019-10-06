79e0e231af166223905df8beb8b352344a32b6fe
Hangjun <ajxuhangjun2007@gmail.com>
2019-10-06 15:28:50 -0700

    Update 236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp

--- 236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
+++ 236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
@@ -64,7 +64,11 @@ public:
 };
 
 /* Remark:
-The above solution traverses the paths twice, and has to store the path. Instead of going from top-dowm, we traverse from the bottom, and once we reach a node which matches one of the two nodes, we pass it up to its parent. The parent would then test its left and right subtree if each contain one of the two nodes. If yes, then the parent must be the LCA and we pass its parent up to the root. If not, we pass the lower node which contains either one of the two nodes (if the left or right subtree contains either p or q), or NULL (if both the left and right subtree does not contain either p or q) up.
+The above solution traverses the paths twice, and has to store the path. Instead of going from top-dowm, we traverse from the 
+bottom, and once we reach a node which matches one of the two nodes, we pass it up to its parent. The parent would then test 
+its left and right subtree if each contain one of the two nodes. If yes, then the parent must be the LCA and we pass its parent 
+up to the root. If not, we pass the lower node which contains either one of the two nodes (if the left or right subtree 
+contains either p or q), or NULL (if both the left and right subtree does not contain either p or q) up.
 */
 /**
  * Definition for a binary tree node.
