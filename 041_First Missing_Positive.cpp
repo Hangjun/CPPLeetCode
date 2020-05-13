44fb547b4e30300ff3f0c6da50465624f1f280a3
Hangjun <ajxuhangjun2007@gmail.com>
2020-05-12 22:17:33 -0700

    Update 041_First Missing_Positive.cpp

--- 041_First Missing_Positive.cpp
+++ 041_First Missing_Positive.cpp
@@ -9,16 +9,18 @@ Your algorithm should run in O(n) time and uses constant space.
 */
 
 /* Analysis:
-First to quote a "Da Shen": given an unsorted array with linear time requirements, a hash table is usually used. Since we are also required 
-to use only constant space, we use the array it self as a hash table: nums: i ---> i+1. Thus if we can get each entry x placed into its 
-"correct" position nums[x-1], then we can locate the first missing positive by a second linear scan. 
+First to quote a "Da Shen": given an unsorted array with linear time requirements, a hash table is usually used. 
+Since we are also required to use only constant space, we use the array it self as a hash table: nums: i ---> i+1. 
+Thus if we can get each entry x placed into its "correct" position nums[x-1], then we can locate the first missing positive by 
+a second linear scan. 
 
-A desired first missing positive, say x, has to satisfy 0 < x <= n, where n = nums.size(). Moreover, if nums[i] = i+1, then x = i+1 is already 
-in the "correct position". If a entry satisfies 0 < nums[i] <= n, and nums[i] != i+1, then we need to move it to its correct position which 
-is nums[nums[i] - 1]. However, after the swap, there are potentially two problems:
-1. The newly swaape in element nums[nums[i] - 1] might still be out of place. Thus we cannot incremnt the cursor i after the swap.
-2. If nums[i] == nums[nums[i] - 1], then if we do the swap without checking this, we will result in an infinite loop: we will keep swapping 
-forever. 
+A desired first missing positive, say x, has to satisfy 0 < x <= n, where n = nums.size(). Moreover, if nums[i] = i+1, then 
+x = i+1 is already in the "correct position". If a entry satisfies 0 < nums[i] <= n, and nums[i] != i+1, then we need to move 
+it to its correct position which is nums[nums[i] - 1]. However, after the swap, there are potentially two problems:
+1. The newly swaape in element nums[nums[i] - 1] might still be out of place. Thus we cannot incremnt the cursor i after the 
+swap.
+2. If nums[i] == nums[nums[i] - 1], then if we do the swap without checking this, we will result in an infinite loop: we will 
+keep swapping forever. 
 
 Therefore we need to check all four conditions before swapping: see line 32.
 */
