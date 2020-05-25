828e7dc1ae46f2c0cdfb8ef263bf2d254f9c8fbd
Hangjun <ajxuhangjun2007@gmail.com>
2020-05-24 21:27:31 -0700

    Update 084. Largest Rectangle in Histogram.cpp

--- 084. Largest Rectangle in Histogram.cpp
+++ 084. Largest Rectangle in Histogram.cpp
@@ -62,13 +62,28 @@ public:
 };
 
 /*
-Remark: You can observe that in the Divide and Conquer Approach, we gain the advantage, since the large problem is divided into substantially smaller subproblems. But, we won't gain much advantage with that approach if the array happens to be sorted in either ascending or descending order, since every time we need to find the minimum number in a large subarray which takes O(n). Thus, the overall complexity becomes O(n^2) in the worst case. We can reduce the time complexity by using a Segment Tree to find the minimum every time which can be done in O(logn) time. The overall time complexity is then reduced to O(nlogn).
+Remark: You can observe that in the Divide and Conquer Approach, we gain the advantage, since the large problem is divided 
+into substantially smaller subproblems. But, we won't gain much advantage with that approach if the array happens to be 
+sorted in either ascending or descending order, since every time we need to find the minimum number in a large subarray which 
+takes O(n). Thus, the overall complexity becomes O(n^2) in the worst case. We can reduce the time complexity by using a 
+Segment Tree to find the minimum every time which can be done in O(logn) time. The overall time complexity is then reduced to 
+O(nlogn).
 */
 
 /*
-Analysis: In this approach, we maintain a stack. Initially, we push a -1 onto the stack to mark the end. We start with the leftmost bar and keep pushing the current bar's index onto the stack until we get two successive numbers in descending order, i.e. until we get a[i-1] > a[i]. Now, we start popping the numbers from the stack until we hit a number stack[j]stack[j] on the stack such that a\big[stack[j]\big] \leq a[i]a[stack[j]]≤a[i]. Every time we pop, we find out the area of rectangle formed using the current element as the height of the rectangle and the difference between the the current element's index pointed to in the original array and the element stack[top-1] -1stack[top−1]−1 as the width i.e. if we pop an element stack[top]stack[top] and i is the current index to which we are pointing in the original array, the current area of the rectangle will be considered as (i-stack[top-1]-1) \times a\big[stack[top]\big](i−stack[top−1]−1)×a[stack[top]].
+Analysis: In this approach, we maintain a stack. Initially, we push a -1 onto the stack to mark the end. We start with the 
+leftmost bar and keep pushing the current bar's index onto the stack until we get two successive numbers in descending order, 
+i.e. until we get a[i-1] > a[i]. Now, we start popping the numbers from the stack until we hit a number stack[j]stack[j] on 
+the stack such that a\big[stack[j]\big] \leq a[i]a[stack[j]]≤a[i]. Every time we pop, we find out the area of rectangle formed 
+using the current element as the height of the rectangle and the difference between the the current element's index pointed 
+to in the original array and the element stack[top-1] -1stack[top−1]−1 as the width i.e. if we pop an element 
+stack[top]stack[top] and i is the current index to which we are pointing in the original array, the current area of the 
+rectangle will be considered as (i-stack[top-1]-1) \times a\big[stack[top]\big](i−stack[top−1]−1)×a[stack[top]].
 
-Further, if we reach the end of the array, we pop all the elements of the stack and at every pop, this time we use the following equation to find the area: (stack[top]-stack[top-1]) \times a\big[stack[top]\big](stack[top]−stack[top−1])×a[stack[top]], where stack[top]stack[top] refers to the element just popped. Thus, we can get the area of the of the largest rectangle by comparing the new area found everytime.
+Further, if we reach the end of the array, we pop all the elements of the stack and at every pop, this time we use the 
+following equation to find the area: (stack[top]-stack[top-1]) \times a\big[stack[top]\big](stack[top]−stack[top−1])×a[stack[top]], 
+where stack[top]stack[top] refers to the element just popped. Thus, we can get the area of the of the largest rectangle by 
+comparing the new area found everytime.
 
 Here is a really nice blog in Chinese that describes this algorithm in detail: http://www.cnblogs.com/lichen782/p/leetcode_Largest_Rectangle_in_Histogram.html.
 
